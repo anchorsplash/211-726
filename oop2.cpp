e06d0036a1bccd4944926fa3817442e23a58aed7
anchorsplash <90962127+anchorsplash@users.noreply.github.com>
2022-03-18 10:39:55 +0300

    Update oop2.cpp

--- oop2.cpp
+++ oop2.cpp
@@ -1,12 +1,12 @@
-﻿#include <iostream>
+#include <iostream>
 #include <iomanip>
 using namespace std;
 
 void task5() {
     int i, l;
     int c = 0;
-    for (i = 1; c < 21; i+=2) {
-        cout << setw(21 - c);
+    for (i = 1; c < 21; i += 2) {
+        cout << setw(21-c);
         for (l = 0; l != i; l += 1) {
             cout << "x";
         }
@@ -19,7 +19,7 @@ int task6() {
     unsigned int numb;
     unsigned long fact = 1;
     cout << "Введите число: ";
-    cin >> numb; 
+    cin >> numb;
     if (numb == 0) {
         cout << "Выходим из программы...";
         return 0;
@@ -33,11 +33,30 @@ int task6() {
 }
 
 void task7() {
-    int y, o;
+    int y;
+    float o, p, s;
     cout << "Введите начальный вклад: ";
+    cin >> s;
     cout << "Введите число лет: ";
-    cout << "Введите процентную ставку";
-    cout << "Через " << y << "лет вы получите" << o << "";
+    cin >> y;
+    cout << "Введите процентную ставку: ";
+    cin >> p;
+    for (int i = 0; i < y; i++) {
+        s += s * (p / 100);
+    }
+    cout << "Через " << y << " лет вы получите " << s << " долларов" << endl;
+
+}
+
+void task8() {
+    int f1, sh1, p1, f2, sh2, p2, f, sh, p;
+    cout << "Введите первую сумму: ";
+    cin >> f1 >> sh1 >> p1;
+    cout << "Введите вторую сумму: ";
+    cin >> f2 >> sh2 >> p2;
+    f = f1 + f2 + (((sh1 + sh2)*12 + p1 + p2)) / 20;
+    sh = (((sh1 + sh2) * 12 + p1 + p2)) / 12;
+    p = (((sh1 + sh2) * 12 + p1 + p2)) % 12;
 
 }
 
@@ -57,6 +76,14 @@ int main() {
         }
         if (n == 6) {
             task6();
+            cout << endl;
+        }
+        if (n == 7) {
+            task7();
+            cout << endl;
+        }
+        if (n == 8) {
+            task8();
             cout << endl;
         }
     }
